ee01095739d072fd21c1249ff201a648b1103e34
Ken Hoover <k.hoover3@gmail.com>
2022-12-03 21:58:34 -0800

    Added day 5 starter.

---
+++ day5/src/main.rs
@@ -0,0 +1,10 @@
+use std::io::BufRead;
+use std::str::FromStr;
+
+use anyhow::{Result, anyhow};
+use common_utils::get_buffered_input;
+use itertools::Itertools;
+
+fn main() -> Result<()> {
+    let input = get_buffered_input();
+}
