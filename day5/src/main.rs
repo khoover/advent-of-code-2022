d7b667933cfa530aedcdbd31fbd752b4738e49ac
Ken Hoover <k.hoover3@gmail.com>
2022-12-04 22:33:03 -0800

    Finished day 5 part 1.

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -7,5 +7,98 @@ use itertools::Itertools;
 
 fn main() -> Result<()> {
     let input = get_buffered_input();
+    let mut lines = input.lines();
+    let mut state = get_initial_state(lines.by_ref())?;
+    lines.map(|line_res| MoveSpec::from_str(&(line_res?)))
+        .fold_while(Ok(()), |_, move_spec| {
+            use itertools::FoldWhile;
+            let move_spec = match move_spec {
+                Ok(spec) => spec,
+                Err(e) => return FoldWhile::Done(Err(e))
+            };
+            match move_spec.do_move(&mut state) {
+                Ok(_) => FoldWhile::Continue(Ok(())),
+                Err(e) => FoldWhile::Done(Err(e))
+            }
+        }).into_inner()?;
+    let output_utf8_bytes: Vec<u8> = state.into_iter()
+        .map(|v| v.last().copied().unwrap_or(b' '))
+        .collect();
+    println!("Top crates are {}", std::str::from_utf8(&output_utf8_bytes)?);
     Ok(())
+}
+
+fn get_initial_state(iter: impl Iterator<Item = std::io::Result<String>>) -> Result<Vec<Vec<u8>>> {
+    let mut lines: Vec<String> = iter.take_while(|line_res| {
+            match line_res {
+                Ok(s) => !s.is_empty(),
+                Err(_) => true
+            }
+        })
+        .try_collect()?;
+    let last_line = lines.pop().ok_or_else(|| anyhow!("Should have at least one line."))?;
+    let num_cols = last_line.split(' ').filter(|s| !s.is_empty()).count();
+    let mut stacks = vec![Vec::new(); num_cols];
+    let push_res = lines.into_iter().rev().try_for_each(|line| {
+        line.as_bytes()
+            .chunks(4)
+            .into_iter()
+            .enumerate()
+            .try_for_each(|(i, chunk)| {
+               match chunk.get(..3) {
+                   Some(&[b' ', b' ', b' ']) => Ok(()),
+                   Some(&[b'[', val, b']']) => {
+                       stacks.get_mut(i).ok_or_else(|| anyhow!("Tried indexing out-of-bounds stack."))?.push(val);
+                       Ok(())
+                   },
+                   _ => Err(anyhow!("Expected [N], got {:?}", chunk))
+               }
+            })
+    });
+    push_res.map(|_| stacks)
+}
+
+struct MoveSpec {
+    count: usize,
+    from: usize,
+    to: usize
+}
+
+impl FromStr for MoveSpec {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        let (
+            _mov,
+            count,
+            _from_text,
+            from,
+            _to_text,
+            to
+        ): (&str, &str, &str, &str, &str, &str) = s.split(' ')
+             .collect_tuple()
+             .ok_or_else(|| anyhow!("Expected 'move N from A to B', got {}", s))?;
+        // skipping checking the text bits, we'll assume they're right
+        Ok(Self {
+            count: count.parse()?,
+            from: from.parse()?,
+            to: to.parse()?
+        })
+    }
+}
+
+impl MoveSpec {
+    fn do_move(&self, stacks: &mut [Vec<u8>]) -> Result<()> {
+        use index_many::generic::{UnsortedIndices, get_many_mut};
+
+        let [src, dst] = get_many_mut(stacks, UnsortedIndices([self.from - 1, self.to - 1]))
+            .ok_or_else(|| anyhow!("From/To are out-of-bounds or the same."))?;
+        for _ in 0..self.count {
+            dst.push(src.pop()
+                     .ok_or_else(|| {
+                         anyhow!("Ran out of elements to pop before move spec done executing.")
+                     })?);
+        }
+        Ok(())
+    }
 }
