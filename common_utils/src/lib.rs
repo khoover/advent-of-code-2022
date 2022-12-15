db7ba9afd69c1ffc364e0421fcbfa0b49324ea07
Ken Hoover <k.hoover3@gmail.com>
2022-12-15 11:19:08 -0800

    Added helper functions for applying nom parsers

--- common_utils/src/lib.rs
+++ common_utils/src/lib.rs
@@ -1,3 +1,6 @@
+use color_eyre::eyre::{eyre, Result};
+use nom::{combinator::all_consuming, error::ParseError, Finish, Parser};
+
 pub fn get_buffered_input() -> std::io::BufReader<std::fs::File> {
     let mut args = std::env::args();
     args.next().unwrap();
@@ -7,4 +10,26 @@ pub fn get_buffered_input() -> std::io::BufReader<std::fs::File> {
         .open(input_path)
         .unwrap();
     std::io::BufReader::new(input_file)
+}
+
+pub fn parse_line<'s, O, E, ParseFn>(line: &'s str, parser: ParseFn) -> Result<O>
+where
+    E: ParseError<&'s str> + std::error::Error,
+    ParseFn: Parser<&'s str, O, E>,
+{
+    match all_consuming(parser)(line).finish() {
+        Ok((_, out)) => Ok(out),
+        Err(e) => Err(eyre!("Parsing error: {}", e)),
+    }
+}
+
+pub fn parse_io_line<O, E, ParseFn>(line: std::io::Result<String>, parser: ParseFn) -> Result<O>
+where
+    E: for<'s> ParseError<&'s str> + std::error::Error,
+    ParseFn: for<'s> Parser<&'s str, O, E>,
+{
+    match line {
+        Ok(s) => parse_line(&s, parser),
+        Err(e) => Err(e.into()),
+    }
 }
