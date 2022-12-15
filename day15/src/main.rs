39e1c712bf6d7e6b99f7ab97451ec0c805b4102f
Ken Hoover <k.hoover3@gmail.com>
2022-12-15 12:36:42 -0800

    Moved sample input const into test function.

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -21,7 +21,8 @@ fn main() -> Result<()> {
 }
 
 #[test]
-const SAMPLE_INPUT: &str = "Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+fn sample() -> Result<()> {
+    const SAMPLE_INPUT: &str = "Sensor at x=2, y=18: closest beacon is at x=-2, y=15
 Sensor at x=9, y=16: closest beacon is at x=10, y=16
 Sensor at x=13, y=2: closest beacon is at x=15, y=3
 Sensor at x=12, y=14: closest beacon is at x=10, y=16
@@ -35,9 +36,6 @@ Sensor at x=17, y=20: closest beacon is at x=21, y=22
 Sensor at x=16, y=7: closest beacon is at x=15, y=3
 Sensor at x=14, y=3: closest beacon is at x=15, y=3
 Sensor at x=20, y=1: closest beacon is at x=15, y=3";
-
-#[test]
-fn sample() -> Result<()> {
     let sensors: Vec<Sensor> = SAMPLE_INPUT
         .lines()
         .map(|line| parse_line(line, parser::sensor))
